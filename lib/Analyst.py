f38d917112f4829c188f51c54519aa0ee1e310af
Lav <78282185+lavnisy@users.noreply.github.com>
2022-11-05 19:07:13 -0300

    Update Analyst.py

--- lib/Analyst.py
+++ lib/Analyst.py
@@ -1,20 +1,27 @@
+#biblioteca que executa os comandos do cmd.
 import subprocess
 
 class Analyst:
 
     def setLayout(self,layout):
         self.layout = layout
-
+        
+    #Método que verifica se os serviços estão ativos.
     def verifyActive(self):
+        #O retorno dessas 2 varíaveis será um informativo dos serviços.
         self.nginxStatus = f"{subprocess.run(['sudo', 'systemctl', 'status', 'nginx'], capture_output=True)}"
         self.mysqlStatus = f"{subprocess.run(['sudo', 'systemctl', 'status', 'mysql'], capture_output=True)}"
+        
+        #Procura o índice da palavra 'active' no informativo.
         activeNginx = self.nginxStatus.find('Active')
         activeMysql = self.mysqlStatus.find('Active')
-
+        
+        #Se existir uma string 'inactive após o índice da palavra 'Active'...
         if 'inactive' in self.nginxStatus[activeNginx:activeNginx + 16]:
             self.nginxStatus = 'off'
         else:
             self.nginxStatus = 'on'
+            #muda o background dos botões.
             self.layout.ids.nginxButton.background_normal = 'media/offnginx.png'
 
         if 'inactive' in self.mysqlStatus[activeMysql:activeMysql + 16]:
@@ -22,6 +29,7 @@ class Analyst:
 
         else:
             self.mysqlStatus = 'on'
+            #muda o background dos botões.
             self.layout.ids.mysqlButton.background_image = 'media/offmysql.png'
 
         return self.layout
@@ -32,13 +40,15 @@ class Analyst:
 
         else:
             self.startNginx()
-
+            
+    #Inicia o servidor nginx e troca a imagem do botão.
     def startNginx(self):
         print('nginx ligado')
         subprocess.run(['sudo','systemctl','start','nginx'],capture_output=True)
         self.layout.ids.nginxButton.background_normal = 'media/offnginx.png'
         self.nginxStatus = 'on'
-
+        
+    #Para o servidor nginx e troca a imagem do botão.
     def stopNginx(self):
         print('nginx desligado')
         subprocess.run(['sudo','systemctl','stop','nginx'],capture_output=True)
@@ -51,12 +61,14 @@ class Analyst:
 
         else:
             self.startMySql()
-
+            
+    #Inicia o serviço mysql e troca a imagem do botão.
     def startMySql(self):
         subprocess.run(['sudo', 'systemctl', 'start', 'mysql'], capture_output=True)
         self.layout.ids.mysqlButton.background_normal = 'media/offmysql.png'
         self.mysqlStatus = 'on'
-
+        
+    #Para o serviço mysql e troca a imagem do botão.
     def stopMySql(self):
         subprocess.run(['sudo', 'systemctl', 'stop', 'mysql'], capture_output=True)
         self.layout.ids.mysqlButton.background_normal = 'media/mysql.png'
